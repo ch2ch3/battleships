5c3c92073e1bf26a633aa6100bc801ddc23a9b4e
Andy010 <andy_gates_010@hotmail.co.uk>
2014-08-26 18:35:01 +0100

    added loader script

---
+++ loader.rb
@@ -0,0 +1,14 @@
+require './lib/ship'
+require './lib/ship_element'
+require './lib/placement_grid'
+require './lib/tracking_grid'
+
+def load
+	@placement_grid = PlacementGrid.new(10, 10)
+	@tracking_grid = TrackingGrid.new(10, 10, @placement_grid)
+	@carrier = Ship.new(:carrier)
+	@battleship = Ship.new(:battleship)
+	@submarine = Ship.new(:submarine)
+	@destroyer = Ship.new(:destroyer)
+	@patrol_boat = Ship.new(:patrol_boat)
+end
