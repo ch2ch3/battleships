e5b21724d1865affee7c80eb671088bac381681d
David Wickes and Ethel Ng <dave.wickes+dave+ethel@gmail.com>
2014-08-26 16:19:30 +0100

    tracking grid up and running - could do with a refactor

---
+++ lib/tracking_grid.rb
@@ -0,0 +1,25 @@
+class TrackingGrid
+
+	attr_reader :dimensions, :placement_grid
+
+	def initialize(x,y, placement_grid)
+		@dimensions = [x,y]
+		@grid = Array.new(x) { Array.new(y) }
+		@placement_grid = placement_grid
+	end
+
+	def fire_at(x,y)
+		raise "You've already fired here!" if coordinate_fired_at?(x,y)
+		@grid[x][y] = (placement_grid.hit?(x,y) ? :hit : :miss)
+		self
+	end
+
+	def coordinate_fired_at?(x,y)
+		not @grid[x][y].nil?
+	end
+
+	def status(x,y)
+		@grid[x][y]
+	end
+
+end
