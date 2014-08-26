327362faabb25cc789047aab7d45c34f87b105f4
Andy010 <andy_gates_010@hotmail.co.uk>
2014-08-26 17:34:59 +0100

    Wrangled the build method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,32 @@
+require 'ship_element'
+
 class Ship
+
+	BUILD_INFO = { 
+		carrier: 5,
+		battleship: 4,
+		submarine: 3,
+		destroyer: 3,
+		patrol_boat: 2,
+		ship: 1
+	}
+
+	attr_reader :elements, :type
+
+	def initialize(type)
+		@elements = []
+		@type = type
+		build
+	end
+
+	def give(element)
+		@elements << element
+	end
+
+	def build
+		BUILD_INFO[@type].times do
+			@elements << ShipElement.new
+		end
+	end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,10 +2,32 @@ require 'ship'
 
 describe Ship do
 
+	let(:ship)   	   	{ Ship.new(:ship) }
+	let(:ship_element) 	{ ShipElement.new}
+	let(:carrier)		{ Ship.new(:carrier) }
+	let(:battleship)	{ Ship.new(:battleship) }
+	let(:submarine)		{ Ship.new(:submarine) }
+	let(:destroyer)		{ Ship.new(:destroyer) }
+	let(:patrol_boat)	{ Ship.new(:patrol_boat) }
+
 context 'Initialization' do
 	it 'can hold ship elements' do
-		
+	expect(ship.elements.count).to eq(1)
+	end
 
+	it 'initializes with the correct number of ship elements for its type' do
+		expect(carrier.elements.count).to eq(5)
+		expect(battleship.elements.count).to eq(4)
+		expect(destroyer.elements.count).to eq(3)
+		expect(submarine.elements.count).to eq(3)
+		expect(patrol_boat.elements.count).to eq(2)
+	end
+
+end
+
+context 'Type' do
+	it 'knows what kind of ship it is' do
+		expect(battleship.type).to eq(:battleship)
 	end
 end
 
