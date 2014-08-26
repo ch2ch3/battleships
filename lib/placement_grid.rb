c05409c4c163460b467d1390c3eef83ee73e83ac
David Wickes <dave.wickes@gmail.com>
2014-08-26 19:07:50 +0100

    hit at metho for placement grid created

--- lib/placement_grid.rb
+++ lib/placement_grid.rb
@@ -1,11 +1,13 @@
 class PlacementGrid
 
+	attr_accessor :grid
+
 	def initialize(x,y)
 		@grid = Array.new(x) { Array.new(y) }
 	end
 
 	def place(ship, x, y, orientation=:horizontal)
-		raise(ArgumentError, "Ship don't fit yo!") if ship_dont_fit?(ship, x, y, orientation)
+		ship_dont_fit?(ship, x, y, orientation)
 		ship.elements.each do |element|
 			@grid[x][y] = element
 			if orientation == :horizontal
@@ -18,13 +20,18 @@ class PlacementGrid
 	end
 
 	def ship_dont_fit?(ship, x, y, orientation)
-		return true unless @grid[x][y].nil?
-		return true if ship.elements.count > x
+		raise (ArgumentError) unless @grid[x][y].nil?
+		raise (ArgumentError) if ship.elements.count > x
 		if orientation == :horizontal
-			return true if ship.elements.count > (@grid.length - x)
+			raise (ArgumentError) if ship.elements.count > (@grid.length - x)
 		elsif orientation == :vertical
-			return true if ship.elements.count > (@grid[x].length - y)
+			raise(ArgumentError) if ship.elements.count > (@grid[x].length - y)
 		end
+	end
+
+	def hit_at?(x,y)
+		return false if @grid[x][y].nil?
+		return true if @grid[x][y].hit!
 	end
 
 end
--- spec/placement_grid_spec.rb
+++ spec/placement_grid_spec.rb
@@ -4,7 +4,7 @@ describe PlacementGrid do
 
 	let(:placement_grid) { PlacementGrid.new(2,2) }
 	let(:ship)			 { double :ship }
-	let(:ship_element)	{ double :ship, :hit!, ship_element}
+	let(:ship_element)	{ double :ship_element }
 
 	it 'is initialized with dimensions' do
 		expect(placement_grid).to eq placement_grid
@@ -23,7 +23,7 @@ describe PlacementGrid do
 
 	it 'can\'t place a ship outside the boundaries of the placement grid' do
 		allow(ship).to receive(:elements).and_return([:ship_element_one, :ship_element_two, :ship_element_three])
-		expect{ placement_grid.place(ship,1,1) }.to raise_error(ArgumentError, "Ship don't fit yo!")
+		expect{ placement_grid.place(ship,1,1) }.to raise_error(ArgumentError)
 	end
 
 	it 'cannot place a 1x2 ship horizontally in the top-right corner of a 9x9 grid' do
@@ -38,8 +38,16 @@ describe PlacementGrid do
 		expect{ placement_grid.place(ship, 0, 7, :vertical)}.to raise_error(ArgumentError)
 	end
 
-	xit 'tells a ship element at a coordinate that it has been hit' do
-		expect(ship_element).to receive(:hit!).and_return
+	it 'tells a ship element at a coordinate that it has been hit' do
+		placement_grid = PlacementGrid.new(9,9)
+		expect(ship_element).to receive(:hit!).and_return(ship_element)
+		placement_grid.grid[3][3] = ship_element
+		expect(placement_grid.hit_at?(3,3)).to be true
+	end
 
+	it 'returns false when there is no ship element at the coordinate being fired at' do
+		placement_grid = PlacementGrid.new(9,9)
+		expect(placement_grid.hit_at?(3,3)).to be false
+	end
 
 end
