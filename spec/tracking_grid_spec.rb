a9c0ec43464926171daaf973c7f67c34bff6d6d9
Andy010 <andy_gates_010@hotmail.co.uk>
2014-08-27 11:28:30 +0100

    Neater tracking grid

--- spec/tracking_grid_spec.rb
+++ spec/tracking_grid_spec.rb
@@ -2,8 +2,8 @@ require 'tracking_grid'
 
 describe TrackingGrid do
 
-	let (:tracking_grid) {TrackingGrid.new(2,2, placement_grid)}
-	let (:placement_grid) {double :placement_grid, hit?: nil}
+	let (:tracking_grid  ) { TrackingGrid.new(2,2, placement_grid) }
+	let (:placement_grid ) { double :placement_grid, hit?: nil     }
 
 	it "is initialized with a dimension" do
 		expect(tracking_grid.dimensions).to eq [2,2]
@@ -14,35 +14,47 @@ describe TrackingGrid do
 	end
 
 	it "can accept a shot" do
-		expect(placement_grid).to receive(:hit_at?).with(1,1)
+		expect(placement_grid).
+			to receive(:hit_at?).
+			with(1,1)
 		expect(tracking_grid.fire_at(1,1)).to be tracking_grid
 	end
 
 	it "tracks shots fired" do
-		expect(placement_grid).to receive(:hit_at?).with(1,1)
+		expect(placement_grid).
+			to receive(:hit_at?).
+			with(1,1)
 		tracking_grid.fire_at(1,1)
 		expect(tracking_grid.coordinate_fired_at?(1,1)).to be true
 	end
 
 	it "will not let you fire at the same place twice" do
-		expect(placement_grid).to receive(:hit_at?).with(1,1)
+		expect(placement_grid).
+			to receive(:hit_at?).
+			with(1,1)
 		tracking_grid.fire_at(1,1)
 		expect{tracking_grid.fire_at(1,1)}.to raise_error
 	end
 
 	it "asks the placement grid if a ship is at a coordinate" do
-		expect(placement_grid).to receive(:hit_at?).with(1,1).and_return(true)
+		expect(placement_grid).
+			to receive(:hit_at?).
+			with(1,1).and_return(true)
 		tracking_grid.fire_at(1,1)
 	end	
 
 	it "sets the coordinate to 'miss' when a shot misses" do
-		allow(placement_grid).to receive(:hit_at?).with(1,1).and_return(false)
+		allow(placement_grid).
+			to receive(:hit_at?).
+			with(1,1).and_return(false)
 		tracking_grid.fire_at(1,1)
 		expect(tracking_grid.status(1,1)).to be :miss
 	end
 
 	it "sets the coordinate to 'hit' when a shot hits a ship" do
-		allow(placement_grid).to receive(:hit_at?).with(1,1).and_return(true)
+		allow(placement_grid).
+			to receive(:hit_at?).
+			with(1,1).and_return(true)
 		tracking_grid.fire_at(1,1)
 		expect(tracking_grid.status(1,1)).to be :hit
 	end
