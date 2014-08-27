d1689e77cb6ac383ac4300326bdc0b6b2a10538e
David Wickes <dave.wickes@gmail.com>
2014-08-27 11:46:42 +0100

    added new test to placement grid for checking elements are where we think they are

--- spec/placement_grid_spec.rb
+++ spec/placement_grid_spec.rb
@@ -31,12 +31,14 @@ describe PlacementGrid do
 	it 'cannot place a ship outside the boundaries of the placement grid' do
 		allow(ship).
 			to receive(:elements).
-			and_return([:ship_element_one,
-								 :ship_element_two,
-								 :ship_element_three
-									])
+			and_return([
+				:ship_element_one,
+				:ship_element_two,
+				:ship_element_three
+			])
 
-		expect{ placement_grid_two_by_two.place(ship,1,1) }.to raise_error(ArgumentError)
+		expect{ placement_grid_two_by_two.place(ship,1,1) }
+			.to raise_error(ArgumentError)
 	end
 
 	it 'cannot place a 1x2 ship horizontally in the top-right corner of a 9x9 grid' do
@@ -44,20 +46,23 @@ describe PlacementGrid do
 			to receive(:elements).
 			and_return([:ship_element_one, :ship_element_two])
 
-		expect{ placement_grid.place(ship, 8, 0)}.to raise_error(ArgumentError)
+		expect{ placement_grid.place(ship, 8, 0)}
+			.to raise_error(ArgumentError)
 	end
 
 	it 'cannot place a 1x5 ship vertically in square 0,7 of a 9x9 grid' do
 		allow(ship).
 			to receive(:elements).
-			and_return([:ship_element_one,
-								 :ship_element_two,
-								 :ship_element_three,
-								 :ship_element_four,
-								 :ship_element_five
-								])
+			and_return([
+				:ship_element_one,
+				:ship_element_two,
+				:ship_element_three,
+				:ship_element_four,
+				:ship_element_five
+			])
 
-		expect{ placement_grid.place(ship, 0, 7, :vertical)}.to raise_error(ArgumentError)
+		expect{ placement_grid.place(ship, 0, 7, :vertical)}
+			.to raise_error(ArgumentError)
 	end
 
 	it 'tells a ship element at a coordinate that it has been hit' do
@@ -68,8 +73,18 @@ describe PlacementGrid do
 	end
 
 	it 'returns false when there is no ship element at the coordinate being fired at' do
-		placement_grid = PlacementGrid.new(9,9)
 		expect(placement_grid.hit_at?(3,3)).to be false
+	end
+
+	it 'places ship elements in the expected grid location' do
+		allow(ship).
+			to receive(:elements).
+			and_return([:ship_element_one, :ship_element_two])
+
+		placement_grid.place(ship,2,2)
+		expect(placement_grid.cell(1,1)).to be nil
+		expect(placement_grid.cell(2,2)).to be :ship_element_one
+		expect(placement_grid.cell(3,2)).to be :ship_element_two
 	end
 
 end
