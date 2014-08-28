28a59f85211b50937a58a2095c53243199ef47b5
Ethel Ng <ethel.ng@gmail.com>
2014-08-28 16:37:59 +0100

    Decoupled player class from ship

--- lib/player.rb
+++ lib/player.rb
@@ -1,27 +1,10 @@
 class Player
 
 	attr_reader :fleet
-  	attr_accessor :firing_board
+  	attr_accessor :firing_board, :ship_board
 
 	def initialize
-		build_fleet
-		@fleet = [@carrier, @battleship, @submarine, @destroyer, @patrol_boat]
-	end
-
-	def ship_board
-		@placement_grid
-	end
-
-	def ship_board=(value)
-		@placement_grid = value
-	end
-
-	def build_fleet
-		@carrier = Ship.new(:carrier)
-		@battleship = Ship.new(:battleship)
-		@submarine = Ship.new(:submarine)
-		@destroyer = Ship.new(:destroyer)
-		@patrol_boat = Ship.new(:patrol_boat)
+		@fleet = []
 	end
 	
 	def dead?
