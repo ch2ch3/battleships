c6ecd33197d76128f6fd2f7f0a711ad8a6186f18
David Wickes <dave.wickes@gmail.com>
2014-08-28 15:38:23 +0100

    board_builder changes

--- spec/board_builder_spec.rb
+++ spec/board_builder_spec.rb
@@ -1,25 +1,29 @@
 require 'board_builder'
+require 'ship_board'
+require 'firing_board'
 
 describe BoardBuilder do
 
 	let(:array) { Array.new(10) { Array.new(10) } }
+	let(:ship_board) { ShipBoard.new }
+	let(:ship_board) { ShipBoard.new }
 
 	context "when it receives a Firing Board" do
 
 		it "can print a table out of a firing board" do
-			table = GridBuilder.new(array)
+			table = BoardBuilder.new(array)
 			expect(puts table)
 		end
 
 		it "changes hits to 'x's" do
 			array = Array.new(10) { Array.new(10, :hit) }
-			table = GridBuilder.new(array)
+			table = BoardBuilder.new(array)
 			expect(puts table)
 		end
 
 		it "changes misses to 'o's" do
 			array = Array.new(10) { Array.new(10, :miss) }
-			table = GridBuilder.new(array)
+			table = BoardBuilder.new(array)
 			expect(puts table)
 		end
 	end
