956f02b53deb893fd241d0e0f2ef61be12e117ca
David Wickes and Ethel Ng <ethel.ng+dave+ethel@gmail.com>
2014-08-28 15:37:48 +0100

    Renamed grid to board

--- spec/board_builder_spec.rb
+++ spec/board_builder_spec.rb
@@ -7,19 +7,19 @@ describe BoardBuilder do
 	context "when it receives a Firing Board" do
 
 		it "can print a table out of a firing board" do
-			table = GridBuilder.new(array)
+			table = BoardBuilder.new(array)
 			expect(puts table)
 		end
 
 		it "changes hits to 'x's" do
 			array = Array.new(10) { Array.new(10, :hit) }
-			table = GridBuilder.new(array)
+			table = BoardBuilder.new(array)
 			expect(puts table)
 		end
 
 		it "changes misses to 'o's" do
 			array = Array.new(10) { Array.new(10, :miss) }
-			table = GridBuilder.new(array)
+			table = BoardBuilder.new(array)
 			expect(puts table)
 		end
 	end
